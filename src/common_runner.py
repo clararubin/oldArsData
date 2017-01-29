0671bd98c82ec58dff499edcf00ac1e785baef47
rubinmarty <rubinmarty@gmail.com>
2017-01-29 11:55:12 -0500

    might have changed like 1 line may as well commit it to be safe

--- src/common_runner.py
+++ src/common_runner.py
@@ -10,16 +10,13 @@ reload(setup)
 reload(graphing)
 reload(pdf_name_generator)
 
-# run make_subplots_each_question over each sec
-def runBySection(PDF_FILENAME_PREFIX, section_list, is_cumulative, is_percent, arrayFiles, questions_data, responses_data):    
-    #sections=[sections[0]]
-    #os.chdir('../../output')
+def runBySection(PDF_FILENAME_PREFIX, section_list, is_cumulative, is_percent, arrayFiles, questions_data, responses_data):
     for i, sec in enumerate(section_list):
         section_iterator = iter(range(sec[0],sec[1]+1))
         
-        filename = pdf_name_generator.generate_pdf_name(PDF_FILENAME_PREFIX, i+1)
+        section_filename = pdf_name_generator.generate_pdf_name(PDF_FILENAME_PREFIX, i+1)
         
         cities, pvals = graphing.make_subplots_each_question(
-                            section_iterator, arrayFiles, filename, is_cumulative, is_percent, questions_data, responses_data
+                            section_iterator, arrayFiles, section_filename, is_cumulative, is_percent, questions_data, responses_data
                             )
     return pvals
